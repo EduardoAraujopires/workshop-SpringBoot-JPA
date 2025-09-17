a35e92126299d49044dd6f743df2c53bf3d53aca
Eduardo Araújo <eduaraujopires370@gmail.com>
2025-09-17 15:26:25 -0300

    User entity and resource

---
+++ src/main/java/com/educandoWed/course/resources/UserResources.java
@@ -0,0 +1,19 @@
+package com.educandoWed.course.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.educandoWed.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+	@GetMapping()
+	public ResponseEntity<User> findAll(){
+		 User u = new User(1L, "Eduardo", "eduardo@gmail.com", "119852142", "123");
+ return ResponseEntity.ok().body(u);
+	}
+}
